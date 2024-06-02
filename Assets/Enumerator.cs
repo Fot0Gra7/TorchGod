e356c1a1ae2181ffe7302b37b3c9c745e36a254a
Fot0Gra7 <stalkermax.228@mail.ru>
2024-06-02 22:16:10 +0300

    1st "Playable prototype"
    Basic functionality +-
    1. Active Ability (Deleting nearby projectiles)
    2. Passive Ability (Deleting ALL projectiles on map)
    3. Health Bar and Ability Bar
    4. Damage System
    5. Main Menu + Pause
    6. Game Over screen
    7. Character Animator
    + Some sprites and misc
    Need to change/add turret system, so they set level to 0 at start of the game, or when player dies/completes game. Currently they don't work properly because of the "Level Enumerator".

--- Assets/Enumerator.cs
+++ Assets/Enumerator.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public enum Level { Level1, Level2, Level3, Level4, Level5 };
+public enum Level { Level0, Level1, Level2, Level3, Level4, Level5 };
 public class LevelUtility {
     public static Level currentLevel;
     public void Next()
